af5c654ce616ab88c695812f404ecfe4e6e693f9
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-19 21:26:02 -0600

    desk test add tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,24 @@ class Deck
     @cards = cards
   end
 
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  def high_ranking_cards
+    @cards.find_all {|card| card.rank >= 11}
+  end
+
+  def percent_high_ranking
+    (( high_ranking_cards.length.to_f / @cards.length.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+
 end
