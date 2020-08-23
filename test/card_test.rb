227f5191bd84729fff6e6f2072cdf32985dc38c3
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-23 17:57:58 -0600

    deleted commented lines

--- test/card_test.rb
+++ test/card_test.rb
@@ -8,14 +8,10 @@ class CardTest < Minitest::Test
   end
 
   def test_it_exists
-    # card = Card.new(:diamond, 'Queen', 12)
-
     assert_instance_of Card, @card
   end
 
   def test_it_has_readable_attributes
-    # skip
-    # @card = Card.new(:diamond, 'Queen', 12)
 
     assert_equal :spade, @card.suit
     assert_equal 'King', @card.value
