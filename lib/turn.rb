5dd06286d460f4d6f41d68169297343d62f1ada2
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-22 23:28:56 -0600

    add Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+class Turn
+
+  attr_reader :player1, :player2, :name, :card
+  def initialize(player1, player2)
+    @name = name
+    @card = card
+    @player1 = player1
+    @player2 = player2
+  end
+end
